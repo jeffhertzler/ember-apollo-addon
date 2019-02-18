4003250fbe312c46fa4aeaf046e85bbd03aa9584
Jeff Hertzler <jeffhertzler@gmail.com>
2019-02-17 21:57:25 -0500

    convert service to es class syntax

--- addon/services/apollo.js
+++ addon/services/apollo.js
@@ -5,28 +5,22 @@ import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { computed } from '@ember/object';
 
-export default Service.extend({
+export default class Apollo extends Service {
+  client = null;
+  cache = new InMemoryCache();
+  link = new HttpLink({ uri: 'graphql' });
+
   init() {
     this._super(...arguments);
 
     const client = this.createApolloClient();
     this.set('client', client);
-  },
-
-  cache: computed(() => new InMemoryCache()),
-  link: computed(
-    () =>
-      new HttpLink({
-        uri: '/graphql',
-      })
-  ),
+  }
 
   createApolloClient() {
     return new ApolloClient({
       link: this.get('link'),
       cache: this.get('cache'),
     });
-  },
-
-  client: null,
-});
+  }
+}
